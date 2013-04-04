c28ca8a0a7ae4bb0c9088155419371ff2777ff6d
linguofeng <linguofeng@msn.com>
2013-04-04 23:31:01 +0800

    Initial Commit

---
+++ objectiveclearn/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  objectiveclearn
+//
+//  Created by 林国锋 on 13-4-4.
+//  Copyright (c) 2013年 http://www.linguofeng.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
